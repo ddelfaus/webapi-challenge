b855aa4c986a5289df59909ead789ec93401ab67
ddelfaus <ddelfaus@gmail.com>
2019-12-06 11:37:31 -0500

    get request

--- Routers/mainRouter.js
+++ Routers/mainRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const projectsDb = require("../data/helpers/projectModel")
+const actionsDb = require("../data/helpers/actionModel")
 
 
 const router = express.Router();
@@ -27,8 +28,39 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.get('/:id', (req, res) => {
+    const id = req.params.id
+    projectsDb.get(id)
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+    
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the users"
+      });
+    });
+  });
+
+  router.get('/:id/actions', (req, res) => {
+    const id = req.params.id
+    actionsDb.get(id)
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+    
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the users"
+      });
+    });
+  });
 
 
+
+  
 
 
   module.exports = router;
