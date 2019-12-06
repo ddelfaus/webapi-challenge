43b654d0b6308332bd414fc33676e460ee276fdc
ddelfaus <ddelfaus@gmail.com>
2019-12-06 11:27:13 -0500

    basic router

--- Routers/mainRouter.js
+++ Routers/mainRouter.js
@@ -1,9 +1,34 @@
 const express = require('express');
 
-// const projectDb = require()
+const projectsDb = require("../data/helpers/projectModel")
 
 
 const router = express.Router();
 
 
 router.use(express.json())
+
+
+//get request
+
+
+router.get('/', (req, res) => {
+ 
+    projectsDb.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+    
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the users"
+      });
+    });
+  });
+
+
+
+
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
-
-
+const mainRouter = require("./Routers/mainRouter")
 
 const server = express();
 
 
 server.use(express.json());
+
+
+
+server.use("/api/projects", mainRouter)
 
 server.get('/', (req, res) => {
     res.send(`<h2>webapi challenge </h2>`);
